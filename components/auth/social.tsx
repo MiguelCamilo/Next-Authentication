42e2b8c6b02cfc58b9e7c1e49d03f4ceb7ab443b
Miguel Camilo <miguelcamilo48@gmail.com>
2024-01-03 19:05:12 -0500

    creating ui for login component

---
+++ components/auth/social.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Social = () => {
+    return (
+        <div className='flex items-center w-full gap-x-2'>
+            
+        </div>
+     );
+}
+ 
+export default Social;
