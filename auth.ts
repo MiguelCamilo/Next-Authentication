73295bb6aa1e87f13c8a11c0ba53c2bb8249824d
Miguel Camilo <miguelcamilo48@gmail.com>
2024-01-14 16:48:10 -0500

    return the user role through jwt token

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from 'next-auth';
-import authConfig from '@/auth.config';
 
 import { db } from '@/lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+
+import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 
 export const {
   handlers: { GET, POST },
@@ -12,15 +14,27 @@ export const {
 } = NextAuth({
   callbacks: {
     
-    async session({ token, session }) {
+    async session({ token, session }) {        
+
+        // this allows for the id thats stored
+        // in the token to be showed in the session        
         if(token.sub && session.user) {
-            session.user.id = token.sub            
-        }
+            session.user.id = token.sub                              
+        }                
         return session
     },
 
     // this returns majority of user data
-    async jwt({ token }) {        
+    async jwt({ token }) {
+
+        if(!token.sub) return token // if !token.sub it means user is logged out
+
+        const exisitingUser = await getUserById(token.sub)
+
+        if(!exisitingUser) return token
+
+        token.role = exisitingUser.role // how to return the logged in user role
+
         return token
     }
   },
